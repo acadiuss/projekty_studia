492c9ea32304ba35780995bbfa112f02954565ef
acadiuss <84239509+acadiuss@users.noreply.github.com>
2023-10-22 10:37:02 +0200

    ver2

---
+++ S/FIFO.cpp
@@ -0,0 +1,9 @@
+//zad . oparta na liscie kolejka FIFO   head i tail poczatek koleji i koniec, jak dodajemy to do tail pobieram#
+#include<iostream>
+using namespace std;
+
+
+class Element
+{
+    
+}
